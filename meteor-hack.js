306e764a3c9ab917c2c7eee59b62422b6e082f2a
Laur Ion <laurion@Laurs-MacBook-Air.local>
2013-10-12 16:27:47 +0300

    dev commit

--- meteor-hack.js
+++ meteor-hack.js
@@ -1,43 +1,29 @@
 if (Meteor.isClient) {
-Session.set("els",[{title:"1"},{title:"2"}]);
+  data = [];
+  for(i=0;i<20;i++){
+    var row=[];
+    for(j=0;j<20;j++){
+      row.push({title:(i+" "+j), body: "test"});
+    }
+    data.push(row);
+  }
+  Session.set("els",data);
 Template.table.element = function(){
 	console.log("wtf");
-	for()
+  
 	return Session.get("els");
 }
-    var col, content, h, i, j, user, w;
-//    console.log('jquery loaded (via assets/js/main.coffee)');
-    content = '';
-    w = 20;
-    h = 20;
-    user = 1;
-    if (user === 1) {
-      col = "#FF0";
-    }
-    if (user === 2) {
-      col = "#F00";
-    }
-    i = 0;
-    while (i < h) {
-      content += "<tr>";
-      j = 0;
-      while (j < w) {
-        content += "<td>" + i + "," + j + " </td>";
-        j++;
-      }
-      content += "</tr>";
-      i++;
-    }
-//    alert(content)
-    $("#tb").html(content);
-    return $("td").click(function() {
-      return $(this).css("background", col);
-    });
-  };
+Template.table.events({
+  "click td": function(event){
+    console.log(this);
+  }
+});
 
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
+
+Items = new Meteor.Collection("items");
+if(Meteor.isServer){
+
+}
+

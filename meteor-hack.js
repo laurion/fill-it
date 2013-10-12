1e9600173b854519aa98e5e514ee1c62c8a0bf1a
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2013-10-12 16:45:43 +0300

    WIP

--- meteor-hack.js
+++ meteor-hack.js
@@ -1,28 +1,30 @@
+Items = new Meteor.Collection("items");
+// Items.insert({data: []})
 if (Meteor.isClient) {
   data = [];
   for(i=0;i<20;i++){
     var row=[];
     for(j=0;j<20;j++){
-      row.push({title:(i+" "+j), body: "test"});
+      row.push({title:(i+" "+j), color: "aa"});
     }
     data.push(row);
   }
+  // Items.update({Session.get("data"):data});
   Session.set("els",data);
-Template.table.element = function(){
-	console.log("wtf");
-  
-	return Session.get("els");
-}
-Template.table.events({
-  "click td": function(event){
-    console.log(this);
+  Template.table.element = function(){
+  	console.log("wtf");
+    
+  	return Session.get("els");
   }
+  Template.table.events({
+    "click td": function(event){
+      a = get[]
+    }
 });
 
 
 }
 
-Items = new Meteor.Collection("items");
 if(Meteor.isServer){
 
 }

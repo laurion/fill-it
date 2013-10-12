49e324f5786561c1c9090713e22ccb96999c6b72
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2013-10-12 17:24:23 +0300

    more users

--- meteor-hack.js
+++ meteor-hack.js
@@ -1,31 +1,42 @@
 Items = new Meteor.Collection("items");
 // Items.insert({data: []})
 if (Meteor.isClient) {
-  data = [];
-  for(i=0;i<20;i++){
-    var row=[];
-    for(j=0;j<20;j++){
-      row.push({title:(i+" "+j), color: "aa"});
-    }
-    data.push(row);
-  }
-  // Items.update({Session.get("data"):data});
-  Session.set("els",data);
   Template.table.element = function(){
-  	console.log("wtf");
-    
-  	return Session.get("els");
+  	item =  Items.findOne();
+    return item;
   }
   Template.table.events({
     "click td": function(event){
-      a = get[]
+      console.log(this);
+      item = Items.findOne();
+      if(item && item.value){
+        item.value[this.i][this.j].color="blue";
+        Items.update({_id:item._id},{value:item.value});
+      }
     }
-});
-
+  });
+  Meteor.subscribe("items");
 
 }
 
 if(Meteor.isServer){
-
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+  Meteor.publish("items", function(){
+    if(!Items.find().count()){
+      console.log("adding items");
+      data = [];
+      for(i=0;i<20;i++){
+        var row=[];
+        for(j=0;j<20;j++){
+          row.push({i:i,j:j, color: ""});
+        }
+        data.push(row);
+      }
+      Items.insert({value:data});
+    }
+    return Items.find();
+  });
 }
 
